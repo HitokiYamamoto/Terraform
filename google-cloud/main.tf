1d6abd418ecb8bb7613746de559fd4d559433265
yamamoto hitoki <brumaire1013@gmail.com>
2026-01-24 17:28:44 +0900

    Pub/Sub作成

--- google-cloud/main.tf
+++ google-cloud/main.tf
@@ -1,4 +1,6 @@
 module "budget_alert" {
-  source     = "./notification"
-  project_id = var.project_id
+  source             = "./notification"
+  project_id         = var.project_id
+  project_number     = var.project_number
+  billing_account_id = var.billing_account_id
 }
