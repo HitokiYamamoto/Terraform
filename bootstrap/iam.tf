f1ecd2f05ef9ab8930883430551363f7b312a6af
yamamoto hitoki <brumaire1013@gmail.com>
2026-01-24 12:37:36 +0900

    fix: trivy対策
    roles/iam.serviceAccountUserをプロジェクトレベルで付与しない

--- bootstrap/iam.tf
+++ bootstrap/iam.tf
@@ -2,11 +2,21 @@ locals {
   service_account_roles = [
     "roles/storage.admin",
     "roles/iam.serviceAccountAdmin",
-    "roles/iam.serviceAccountUser", # サービスアカウントの利用権限
     "roles/artifactregistry.admin",
     "roles/cloudfunctions.admin",
     "roles/resourcemanager.projectIamAdmin", # プロジェクトレベルのIAMポリシーを変更する権限
   ]
+  # dataリソースで取得したいサービスアカウント名のリスト
+  target_service_account_names = [
+    "budget-alert-function"
+  ]
+}
+
+# 特定のサービスアカウントをdataリソースで取得
+data "google_service_account" "targets" {
+  for_each   = toset(local.target_service_account_names)
+  account_id = each.value
+  project    = var.project_id
 }
 
 # ユーザーアカウントにサービスアカウントのimpersonate権限を付与
@@ -15,6 +25,18 @@ resource "google_service_account_iam_binding" "token_creator" {
   role               = "roles/iam.serviceAccountTokenCreator"
 
   members = var.impersonators
+}
+
+# 特定のサービスアカウントに対してのみroles/iam.serviceAccountUserを付与
+resource "google_service_account_iam_binding" "service_account_user" {
+  for_each = data.google_service_account.targets
+
+  service_account_id = each.value.id
+  role               = "roles/iam.serviceAccountUser"
+
+  members = [
+    module.default_service_account.member,
+  ]
 }
 
 # サービスアカウントに各種管理権限を付与
